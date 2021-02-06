7933e929e753aae981c9531fd0ea60656b88160c
achernenkov <jardagorng@gmail.com>
2021-02-06 21:37:44 +0300

    Completed page Contacts

--- src/components/sitePages/contacts/contacts.tsx
+++ src/components/sitePages/contacts/contacts.tsx
@@ -3,7 +3,14 @@ import React from "react";
 function Contacts(){
     return (
         <div className='pageWrapper'>
-            <div className='contentWrapper'>Content</div>
+            <div className='contentWrapper'>
+                <h1 className='contanctTitle'>My Contact</h1>
+                <div className='contants'>
+                    <p><a href='https://www.linkedin.com/in/andrey-chernenkov-b35926200/'>LinkIden</a></p>
+                    <p><a href='https://t.me/andry_y'>Telegram (@andry_y)</a></p>
+                    <p>Email: chernenkovdeveloper@gmail.com</p>
+                </div>
+            </div>
         </div>
     )
 }
